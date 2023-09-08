d02d270139a232ddf8f88f1cb290dbf37daa5b73
Anas0501 <mohamadanas0501@gmail.com>
2023-09-08 23:05:25 +0530

    11 - Admin Functionality Done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,10 +14,11 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PrivateRoute from './components/PrivateRoute'
+import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+import CartScreen from './screens/CartScreen'; 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
@@ -25,6 +26,11 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import OrderListScreen from './screens/admin/OrderListScreen';
+import ProductListScreen from './screens/admin/ProductListScreen'; 
+import ProductEditScreen from './screens/admin/ProductEditScreen';
+import UserListScreen from './screens/admin/UserListScreen';
+import UserEditScreen from './screens/admin/UserEditScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -43,7 +49,15 @@ const router = createBrowserRouter(
         <Route path='/order/:id' element={<OrderScreen />} />
         <Route path='/profile' element={<ProfileScreen />} />
       </Route>
-    </Route>
+      {/* Admin users */}
+     <Route path='' element={<AdminRoute />}>
+     <Route path='/admin/orderlist' element={<OrderListScreen />} />
+     <Route path='/admin/productlist' element={<ProductListScreen />} />
+     <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+     <Route path='/admin/userlist' element={<UserListScreen />} />
+     <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+   </Route>
+  </Route>   
   )
 );
 
